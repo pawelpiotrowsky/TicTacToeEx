8f9a40a780bd981227c0655072610ff45beadbfd
pawelpiotrowsky <pawelpio96@gmail.com>
2021-04-18 14:25:15 +0200

    winning conditions added.

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -1,8 +1,8 @@
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class TicTacToe {
-
+static ArrayList<Integer> playerPosition = new ArrayList<Integer>();
+static ArrayList<Integer> cpuPosition = new ArrayList<Integer>();
 
     public static void main(String[] args) {
         char[][] gameBoard = {{' ', '|', ' ', '|', ' '},
@@ -20,18 +20,50 @@ public class TicTacToe {
             int cpuPosition = random.nextInt(9) + 1;
             placePiece(gameBoard, cpuPosition, "cpu");
             displayGameBoard(gameBoard);
+            checkWinner();
         }
-        if()
+
 
     }
 
+    private static void checkWinner() {
+        List topRow = Arrays.asList(1, 2, 3);
+        List midRow = Arrays.asList(4, 5, 6);
+        List botRow = Arrays.asList(7, 8, 9);
+        List leftCol = Arrays.asList(1, 4, 7);
+        List midCol = Arrays.asList(2, 5, 8);
+        List rightCol = Arrays.asList(3, 6, 9);
+        List leftToRightX= Arrays.asList(1, 5, 9);
+        List rightToLeftX = Arrays.asList(3, 5, 7);
+
+        List<List> winning = new ArrayList<List>();
+        winning.add(topRow);
+        winning.add(midRow);
+        winning.add(botRow);
+        winning.add(leftCol);
+        winning.add(midCol);
+        winning.add(rightCol);
+        winning.add(leftToRightX);
+        winning.add(rightToLeftX);
+        for (List l: winning){
+            if(playerPosition.containsAll(l)){
+                System.out.println("Congratulations, you won!");
+            }else if(cpuPosition.containsAll(l)){
+                System.out.println("Sorry, you lost :(");
+            }else if(playerPosition.size() + cpuPosition.size() == 9){
+                System.out.println("Board full ,tie");
+            }
+        }
+    }
 
 
     private static void placePiece(char[][] gameBoard, int pos, String user) {
         char symbol = ' ';
         if(user.equals("player")){
             symbol = 'X';
+            playerPosition.add(pos);
         }else if(user.equals("cpu")){
+            cpuPosition.add(pos);
             symbol = 'O';
         }
         switch(pos) {
